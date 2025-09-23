906d9b93314f28a3cdde8a845d88507a90ec963f
AhmetY. <93879643+dubnium0@users.noreply.github.com>
2025-09-23 04:02:57 +0300

    syntax fixed

--- tr-2025.sql
+++ tr-2025.sql
@@ -1,7 +1,6 @@
 SELECT week, region_name, term, score
 FROM `bigquery-472018.turky_term.turkey_top_terms` 
-WHERE 
-  EXTRACT(YEAR FROM week) = 2025
+WHERE EXTRACT(YEAR FROM week) = 2025
 ORDER BY
   week DESC,
   score DESC;
